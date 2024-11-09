8a7dc826e9575a777592a455f54ca6088443d913
Ayush Jain <ayushjain2205@gmail.com>
2024-11-10 01:41:25 +0700

    Add action buttons

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -11,7 +11,16 @@ import { Card, CardContent } from "@/components/ui/card";
 import { client, wallet } from "./constants";
 import { AutoConnect } from "thirdweb/react";
 import Link from "next/link";
-import { Eye, EyeOff, User, Copy, Check } from "lucide-react";
+import {
+  Eye,
+  EyeOff,
+  User,
+  Copy,
+  Check,
+  ArrowUp,
+  ArrowDown,
+  CreditCard,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -51,74 +60,116 @@ export default function Home() {
 
       <div className="p-4">
         <AutoConnect client={client} wallets={[wallet]} />
-        <div className="flex flex-col gap-4 justify-center mb-20">
+        <div className="flex flex-col gap-4 justify-center mb-6">
           {account ? (
-            <Card className="rounded-2xl border-4 border-black overflow-hidden bg-gradient-to-br from-yellow-400 to-yellow-500 relative">
-              <CardContent className="p-6 space-y-6 relative">
-                <div className="flex justify-between items-start">
-                  <div className="flex items-center space-x-2">
-                    <User className="w-5 h-5 text-black" />
-                    <div className="font-mono text-lg font-bold text-black break-all">
-                      {shortenAddress(account.address)}
+            <>
+              <Card className="rounded-2xl border-4 border-black overflow-hidden bg-gradient-to-br from-yellow-400 to-yellow-500 relative">
+                <CardContent className="p-6 space-y-6 relative">
+                  <div className="flex justify-between items-start">
+                    <div className="flex items-center space-x-2">
+                      <User className="w-5 h-5 text-black" />
+                      <div className="font-mono text-lg font-bold text-black break-all">
+                        {shortenAddress(account.address)}
+                      </div>
+                      <Button
+                        variant="outline"
+                        size="icon"
+                        className="rounded-full border-2 border-black bg-white hover:bg-yellow-100 p-1"
+                        onClick={copyAddress}
+                      >
+                        {copySuccess ? (
+                          <Check className="w-2 h-2" />
+                        ) : (
+                          <Copy className="w-2 h-2" />
+                        )}
+                      </Button>
                     </div>
                     <Button
                       variant="outline"
                       size="icon"
-                      className="rounded-full border-2 border-black bg-white hover:bg-yellow-100 p-1"
-                      onClick={copyAddress}
+                      className="rounded-full border-2 border-black bg-white hover:bg-yellow-100 absolute top-2 right-2"
+                      onClick={() => setShowBalance(!showBalance)}
                     >
-                      {copySuccess ? (
-                        <Check className="w-2 h-2" />
+                      {showBalance ? (
+                        <EyeOff className="w-4 h-4" />
                       ) : (
-                        <Copy className="w-2 h-2" />
+                        <Eye className="w-4 h-4" />
                       )}
                     </Button>
                   </div>
+                  <div className="space-y-2">
+                    <div className="text-lg font-bold text-black">
+                      Total Balance
+                    </div>
+                    <div className="flex items-baseline gap-2">
+                      <div className="text-4xl font-black text-black tabular-nums">
+                        {showBalance
+                          ? data?.displayValue.slice(0, 6)
+                          : "••••••"}
+                      </div>
+                      <div className="text-xl font-bold text-black">BNB</div>
+                    </div>
+                    <div className="text-lg font-medium text-black/80">
+                      ≈ ${showBalance ? "321.45" : "••••••"} USD
+                    </div>
+                  </div>
+
                   <Button
                     variant="outline"
-                    size="icon"
-                    className="rounded-full border-2 border-black bg-white hover:bg-yellow-100 absolute top-2 right-2"
-                    onClick={() => setShowBalance(!showBalance)}
-                  >
-                    {showBalance ? (
-                      <EyeOff className="w-4 h-4" />
-                    ) : (
-                      <Eye className="w-4 h-4" />
-                    )}
-                  </Button>
-                </div>
-                <div className="space-y-2">
-                  <div className="text-lg font-bold text-black">
-                    Total Balance
-                  </div>
-                  <div className="flex items-baseline gap-2">
-                    <div className="text-4xl font-black text-black tabular-nums">
-                      {showBalance ? data?.displayValue.slice(0, 6) : "••••••"}
-                    </div>
-                    <div className="text-xl font-bold text-black">BNB</div>
-                  </div>
-                  <div className="text-lg font-medium text-black/80">
-                    ≈ ${showBalance ? "321.45" : "••••••"} USD
-                  </div>
-                </div>
-
-                <Button
-                  variant="outline"
-                  className="rounded-full w-10 h-10 p-0 border-2 border-black bg-white hover:bg-yellow-100 overflow-hidden absolute bottom-2 right-2"
-                  onClick={() =>
-                    setNetwork(network === "BSC" ? "Opbnb" : "BSC")
-                  }
-                >
-                  <img
-                    src={
-                      network === "BSC" ? "/bnb-logo.png" : "/opbnb-logo.png"
+                    className="rounded-full w-10 h-10 p-0 border-2 border-black bg-white hover:bg-yellow-100 overflow-hidden absolute bottom-2 right-2"
+                    onClick={() =>
+                      setNetwork(network === "BSC" ? "Opbnb" : "BSC")
                     }
-                    alt={network}
-                    className="w-full h-full object-cover"
-                  />
+                  >
+                    <img
+                      src={
+                        network === "BSC" ? "/bnb-logo.png" : "/opbnb-logo.png"
+                      }
+                      alt={network}
+                      className="w-full h-full object-cover"
+                    />
+                  </Button>
+                </CardContent>
+              </Card>
+
+              {/* Simple Quick Action Buttons */}
+              <div className="grid grid-cols-3 gap-4 mt-4">
+                <Button className="flex flex-col items-center justify-between p-4 h-24 bg-white hover:bg-yellow-100 border-2 border-black rounded-xl">
+                  <img src="/send.svg" alt="send" className="w-10 h-10" />
+                  <span className="text-sm font-bold text-black">Send</span>
                 </Button>
-              </CardContent>
-            </Card>
+
+                <Button className="flex flex-col items-center justify-between p-4 h-24 bg-white hover:bg-yellow-100 border-2 border-black rounded-xl">
+                  <img src="/receive.svg" alt="receive" className="w-10 h-10" />
+                  <span className="text-sm font-bold text-black">Receive</span>
+                </Button>
+
+                <Button className="flex flex-col items-center justify-between p-4 h-24 bg-white hover:bg-yellow-100 border-2 border-black rounded-xl">
+                  <img src="/buy.svg" alt="buy" className="w-12 h-12 " />
+                  <span className="text-sm font-bold text-black mt-[10px]">
+                    Buy
+                  </span>
+                </Button>
+                <Button className="flex flex-col items-center justify-between p-4 h-24 bg-white hover:bg-yellow-100 border-2 border-black rounded-xl">
+                  <img src="/bridge.svg" alt="bridge" className="w-12 h-12" />
+                  <span className="text-sm font-bold text-black">Bridge</span>
+                </Button>
+
+                <Button className="flex flex-col items-center justify-between p-4 h-24 bg-white hover:bg-yellow-100 border-2 border-black rounded-xl">
+                  <img src="/swap.svg" alt="swap" className="w-14 h-14" />
+                  <span className="text-sm font-bold text-black">Swap</span>
+                </Button>
+
+                <Button className="flex flex-col items-center justify-between p-4 h-24 bg-white hover:bg-yellow-100 border-2 border-black rounded-xl">
+                  <img
+                    src="/activity.svg"
+                    alt="activity"
+                    className="w-10 h-10"
+                  />
+                  <span className="text-sm font-bold text-black">Activity</span>
+                </Button>
+              </div>
+            </>
           ) : (
             <div className="flex flex-col gap-2 items-center justify-center">
               <img
@@ -130,39 +181,7 @@ export default function Home() {
             </div>
           )}
         </div>
-        <Menu />
       </div>
     </main>
-  );
-}
-
-function Menu() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <MenuItem
-        title="Sponsored transactions"
-        href="/gasless"
-        description="Execute transactions without requiring users to hold ETH."
-      />
-      <MenuItem
-        title="Pay"
-        href="/pay"
-        description="Allow users to purchase NFT's using fiat"
-      />
-    </div>
-  );
-}
-
-function MenuItem(props: { title: string; href: string; description: string }) {
-  return (
-    <Link
-      href={props.href}
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </Link>
   );
 }
