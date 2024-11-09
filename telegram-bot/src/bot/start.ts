f16c3c4753482b8e7dba5297a6934f38dd9ed605
Ayush Jain <ayushjain2205@gmail.com>
2024-11-09 15:06:00 +0700

    Improve generate wallet experience

--- telegram-bot/src/bot/start.ts
+++ telegram-bot/src/bot/start.ts
@@ -1,21 +1,23 @@
-import { Composer, InlineKeyboard } from 'grammy'
-import type { Context } from './context.js'
-import { privateKeyToAccount } from 'thirdweb/wallets'
-import { createThirdwebClient } from 'thirdweb'
-import { config } from 'dotenv' 
-config()
+import { Composer, InlineKeyboard } from "grammy";
+import type { Context } from "./context.js";
+import { privateKeyToAccount } from "thirdweb/wallets";
+import { createThirdwebClient } from "thirdweb";
+import { config } from "dotenv";
+config();
 
-const composer = new Composer<Context>()
+const composer = new Composer<Context>();
 
-const feature = composer.chatType('private')
+const feature = composer.chatType("private");
 
 const adminAccount = privateKeyToAccount({
   privateKey: process.env.ADMIN_SECRET_KEY as string,
-  client: createThirdwebClient({ clientId: process.env.THIRDWEB_CLIENT_ID as string }),
-})
+  client: createThirdwebClient({
+    clientId: process.env.THIRDWEB_CLIENT_ID as string,
+  }),
+});
 
-feature.command('start', async (ctx) => {
-  const username = ctx.from?.id+"";
+feature.command("start", async (ctx) => {
+  const username = ctx.from?.id + "";
   const expiration = Date.now() + 600_000; // valid for 10 minutes
   const message = JSON.stringify({
     username,
@@ -24,8 +26,13 @@ feature.command('start', async (ctx) => {
   const authCode = await adminAccount.signMessage({
     message,
   });
-  const keyboard = new InlineKeyboard().webApp('thirdweb App', `${process.env.FRONTEND_APP_ORIGIN}/login/telegram?signature=${authCode}&message=${encodeURI(message)}`);
-  return ctx.reply('Pick an app to launch.', { reply_markup: keyboard })
-})
+  const keyboard = new InlineKeyboard().webApp(
+    "thirdweb App",
+    `${
+      process.env.FRONTEND_APP_ORIGIN
+    }/login/telegram?signature=${authCode}&message=${encodeURI(message)}`
+  );
+  return ctx.reply("Pick an app to launch.", { reply_markup: keyboard });
+});
 
-export { composer as startFeature }
+export { composer as startFeature };
